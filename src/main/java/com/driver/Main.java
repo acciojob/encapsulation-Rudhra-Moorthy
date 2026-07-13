db8341e69d3763f2538c21a50d179ad4d425e2b9
Rudhra-Moorthy <rudhram54@gmail.com>
2026-07-13 21:32:15 +0530

    Encapsulation assignment is done.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+        /* rw.name = "Rudhra";  -- java: name has private access in com.driver.RWOnly */
+
+        rw.setName("Rudhra Moorthy");
+        System.out.println(rw.getName());
+    }
   
 }
